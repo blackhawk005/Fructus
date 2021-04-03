9fcc5a51f9ce59e8a5bb4436f878ca3ad61b3240
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2021-04-04 02:44:03 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Shinit Shetty on 04/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
